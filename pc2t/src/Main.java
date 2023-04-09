91f71166081c779a3f90e05b2c116b110e6895c8
MarekKoziak <240933@vut.cz>
2023-04-09 15:59:49 +0200

    Preparation of main select function

--- pc2t/src/Main.java
+++ pc2t/src/Main.java
@@ -1,7 +1,62 @@
+import java.util.Scanner;
+
 // Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int select;
+        Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("1. Add new film");
+            System.out.println("2. Adjust film");
+            System.out.println("3. Delete film");
+            System.out.println("4. Add rating");
+            System.out.println("5. Film list");
+            System.out.println("6. Search film");
+            System.out.println("7. List of actors or animators, who played in more than 1 film");
+            System.out.println("8. List of films searched by actor or animator");
+            System.out.println("9. Save film info into file");
+            System.out.println("10. Load film info from file");
+            System.out.println("11. Exit app");
+
+            select = sc.nextInt();
+
+            switch (select) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 11:
+
+                    break;
+            }
+        }
     }
 }

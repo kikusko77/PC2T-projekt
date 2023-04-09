fd4d47e679d5195f0bcef3a7e306677cd2996327
MarekKoziak <240933@vut.cz>
2023-04-09 18:48:35 +0200

    Added list of ratings

--- pc2t/src/Film.java
+++ pc2t/src/Film.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Film {
     private String title;
     private String director;
     private int releaseYear;
+    private List<Rating> ratings;
 
     public Film(String title, String director, int releaseYear) {
         this.title = title;
         this.director = director;
         this.releaseYear = releaseYear;
+        this.ratings = new ArrayList<>();
     }
 
     public String getTitle() {
@@ -19,5 +24,9 @@ public abstract class Film {
 
     public int getReleaseYear() {
         return releaseYear;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
     }
 }

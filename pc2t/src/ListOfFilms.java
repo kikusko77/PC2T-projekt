26c7b732ca31e652c698d0115f2aa2aa3a7add77
kikusko77 <christian.kuric@gmail.com>
2023-04-09 16:08:10 +0200

    listoffilms

---
+++ pc2t/src/ListOfFilms.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListOfFilms {
+    private ArrayList<Film> listOfFilms;
+
+    public  ListOfFilms() {
+        this.listOfFilms = new ArrayList<>();
+    }
+
+    public void addFilm(){
+
+        Scanner input = new Scanner(System.in);
+        int movieType = input.nextInt();
+        input.nextLine();
+
+
+    }
+}
